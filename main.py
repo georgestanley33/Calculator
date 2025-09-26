1185861f91e904e156ac9c7a7edeca6493460709
George <georgestanley33@gmail.com>
2025-09-26 21:42:14 +0100

    Add addition

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Calculator")
+print("")
+
+function = input("Which function would you like to carry out? Please enter addition, subtraction, multiplication or division")
+
+num1 = int(input("Enter number 1"))
+num2 = int(input("Enter number 2"))
+
+if function == "addition":
+    print("The answer is " , (num1 + num2))
+
