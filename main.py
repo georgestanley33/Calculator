6e1844147351505965bee13e1328303cd68d6121
George <georgestanley33@gmail.com>
2025-09-26 22:12:40 +0100

    Allow for floats

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ while True:
 
     function = input("Which function would you like to carry out? Please enter addition, subtraction, multiplication or division")
 
-    num1 = int(input("Enter number 1"))
-    num2 = int(input("Enter number 2"))
+    num1 = float(input("Enter number 1"))
+    num2 = float(input("Enter number 2"))
 
     if function == "addition":
         print("The answer is " , (num1 + num2))
